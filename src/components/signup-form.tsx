b50fe86e83a21a6d609ae1344f72e839eba408da
Davematteer <davematteer@gmail.com>
2025-09-23 00:07:14 +0000

    setting up project

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -118,7 +118,7 @@ export function SignUpForm({
             </div>
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
-              <Link href="/login" className="underline underline-offset-4">
+              <Link href="/auth/login" className="underline underline-offset-4">
                 Login
               </Link>
             </div>
