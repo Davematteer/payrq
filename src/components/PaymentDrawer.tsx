9931af0c45811ead92cf42461a9f395426cef390
Davematteer <davematteer@gmail.com>
2025-09-24 14:17:35 +0000

    bruh whats going on with the CICD

--- src/components/PaymentDrawer.tsx
+++ src/components/PaymentDrawer.tsx
@@ -11,6 +11,7 @@ import {
 
 
 import { Button } from "./ui/button";
+import { CheckCheck, ShoppingCart } from "lucide-react";
 
 
 
@@ -21,11 +22,11 @@ export function PaymentDrawer({isOpen, setOpen}:{isOpen:boolean, setOpen: (v:boo
         <Drawer open={isOpen} onOpenChange={setOpen} >
             <DrawerContent>
                 <DrawerHeader>
-                    <DrawerTitle>Are you absolutely sure?</DrawerTitle>
-                    <DrawerDescription>This action cannot be undone.</DrawerDescription>
+                    <DrawerTitle className="flex justify-center items-center gap-2 text-2xl font-extralight"><ShoppingCart /> </DrawerTitle>
+                    <DrawerDescription>Confirm Payment!</DrawerDescription>
                 </DrawerHeader>
             <DrawerFooter>
-                <Button>Submit</Button>
+                <Button className="flex justify-center items-center gap-2 ">Check Out <CheckCheck /></Button>
                 <DrawerClose asChild>
                 <Button>Cancel</Button>
                 </DrawerClose>
